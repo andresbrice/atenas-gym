7c207d013ac062e8099e0a3c7c746a8effbdb536
andresbrice <andresbriceno71094@gmail.com>
2021-06-07 00:08:09 -0300

    readme modificado | add user admin seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,7 +14,33 @@ class UserSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
+    { 
+      DB::table('users')->insert([
+        'name' => "admin",
+        "email" => "admin@admin.com",
+        "userName"=> "admin",
+        "dni" => "12345678",
+        "lastName" => "admin",
+        "age" => 27,
+        "gender" => "Masculino",
+        "phone" => "12345",
+        "emergency_number" => "12345",
+        "role_id" => 3,
+        "eRespiratorias" => 0,
+        "eCardiacas"=> 0,
+        "eRenal"=> 0,
+        "epilepsia"=> 0,
+        "convulsiones"=> 0,
+        "diabetes"=> 0,
+        "asma"=> 0,
+        "alergia"=> 0,
+        "medicacion"=> 0,
+        "new_password"=>0,
+        "email_verified_at"=>null,
+        "password" => Hash::make('grupo2utnconcordia'),
+        "remember_token"=>null
+      ]);
+
         User::factory()->times(50)->create();
     }
 }
