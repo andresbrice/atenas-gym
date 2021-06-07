2c7d0d8c3ea5976d219ec26f612cbc6feec9553f
andresbrice <andresbriceno71094@gmail.com>
2021-06-07 02:09:19 -0300

    init modelos y controladores Alum y prof

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,75 +9,88 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasFactory, Notifiable;
+  use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'userName',
-        'dni',
-        'lastName',
-        'age',
-        'gender',
-        'phone',
-        'emergency_number',
-        'role_id',
-        'eRespiratorias',
-        'eCardiacas',
-        'eRenal',
-        'epilepsia',
-        'convulsiones',
-        'diabetes',
-        'asma',
-        'alergia',
-        'medicacion',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'name',
+    'email',
+    'userName',
+    'dni',
+    'lastName',
+    'age',
+    'gender',
+    'phone',
+    'emergency_number',
+    'role_id',
+    'eRespiratorias',
+    'eCardiacas',
+    'eRenal',
+    'epilepsia',
+    'convulsiones',
+    'diabetes',
+    'asma',
+    'alergia',
+    'medicacion',
+  ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that should be cast to native types.
+   *
+   * @var array
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
 
-    public function role()
-    {
-        return $this->belongsTo(Role::class, 'role_id');
+  public function role()
+  {
+    return $this->belongsTo(Role::class, 'role_id');
+  }
+
+  public function alumno()
+  {
+    return $this->hasOne(Alumno::class);
+  }
+
+  public function profesor()
+  {
+    return $this->hasOne(Profesor::class);
+  }
+
+  // QUERY SCOPES
+  public function scopeUserName($query, $userName)
+  {
+    if ($userName) {
+      return $query->where('userName', "LIKE", "%$userName%");
     }
+  }
 
-    // QUERY SCOPES
-    public function scopeUserName($query,$userName){
-      if ($userName) {
-        return $query->where('userName',"LIKE","%$userName%");
-      } 
+  public function scopeName($query, $name)
+  {
+    if ($name) {
+      return $query->where('name', "LIKE", "%$name%");
     }
+  }
 
-    public function scopeName($query,$name){
-      if ($name) {
-        return $query->where('name',"LIKE","%$name%");
-      }
+  public function scopeLastName($query, $lastName)
+  {
+    if ($lastName) {
+      return $query->where('lastName', "LIKE", "%$lastName%");
     }
-
-    public function scopeLastName($query,$lastName){
-      if ($lastName) {
-        return $query->where('lastName',"LIKE","%$lastName%");
-      } 
-    }
+  }
 }
