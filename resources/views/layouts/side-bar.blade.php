bbe2ab36f3288e2701aad9843bdbc226553832c0
Jandrih <julianandrichf@gmail.com>
2021-06-07 18:07:39 -0300

    Iniciando rama Dashboard

--- resources/views/layouts/side-bar.blade.php
+++ resources/views/layouts/side-bar.blade.php
@@ -1,5 +1,5 @@
 <div id="atenas-sidebar"
-  class="bg-gray-400 h-16 w-1/5 z-10 fixed bottom-0 mt-12 border-r border-red-800 md:relative md:h-full md:w-52 max-w-screen-md">
+  class="bg-gray-400 h-16 w-1/5 z-10 fixed bottom-0 mt-12 border-r border-red-800 md:relative md:h-full md:w-52">
   <div class="md:mt-12 md:w-48 md:fixed md:left-0 md:top-0 content-center md:content-start text-left justify-between">
     @switch(Auth::user()->role_id)
     @case(1)
@@ -14,5 +14,8 @@
     @default
     @break
     @endswitch
+
+
+    Holis
   </div>
 </div>
