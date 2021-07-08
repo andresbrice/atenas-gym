6fe4435b36eabf70e283fe364ea229dc09363aea
andresbrice <andresbriceno71094@gmail.com>
2021-07-08 19:31:12 -0300

    user index fixed

--- resources/views/usuario/index.blade.php
+++ resources/views/usuario/index.blade.php
@@ -50,7 +50,118 @@
                 {{-- FIN BUSCADOR --}}
               </div>
             </div>
+            <x-table>
+              @section('nombre-columna')
+              <tr>
+                <th scope="col"
+                  class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  usuario
+                </th>
+                <th scope="col"
+                  class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  nombre
+                </th>
+                <th scope="col"
+                  class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  apellido
+                </th>
+                <th scope="col"
+                  class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  email
+                </th>
+                <th scope="col"
+                  class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  rol
+                </th>
+                <th scope="col"
+                  class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  acciones
+                </th>
+              </tr>
+              @endsection
+
+
+              @section('contenido-filas')
+              @forelse ($usuarios as $usuario)
+              <tr>
+                <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
+                  {{ $usuario->userName }}
+                </td>
+
+                <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
+                  {{ $usuario->name }}
+                </td>
+
+                <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
+                  {{ $usuario->lastName }}
+                </td>
+
+                <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
+                  {{ $usuario->email }}
+                </td>
+
+                <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
+                  {{ $usuario->role->nombre_rol }}
+                </td>
+
+                <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
+                  <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                      <x-button
+                        class="outline-none focus:outline-none border px-3 py-1 bg-gray-900 hover:bg-gray-700 text-white rounded-sm flex items-center min-w-32">
+                        <span class="pr-1 font-semibold flex-1">Acciones</span>
+                        <span>
+                          <svg
+                            class="fill-current h-4 w-4 transform group-hover:-rotate-180
+                                                                                              transition duration-150 ease-in-out"
+                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                          </svg>
+                        </span>
+                      </x-button>
+                    </x-slot>
+
+                    <x-slot name="content">
+                      <x-dropdown-link href="{{ route('usuario.edit', $usuario->id) }}">
+                        {{ __('Edit') }}
+                      </x-dropdown-link>
+
+                      <form method="POST" action="{{ route('usuario.destroy', $usuario->id) }}">
+                        @csrf
+                        @method('DELETE')
+
+                        <x-dropdown-link class="text-center" :href="route('usuario.destroy',$usuario->id)">
+                          <button
+                            onclick="return confirm('¿Esta seguro de querer borrar este usuario?');">Borrar</button>
+                        </x-dropdown-link>
+                      </form>
+
+                      <x-dropdown-link href="{{ route('usuario.show', $usuario->id) }}">
+                        {{ __('Show') }}
+                      </x-dropdown-link>
+                    </x-slot>
+                  </x-dropdown>
+                </td>
+              </tr>
+              @empty
+              <tr>
+                <td>
+                  <center>
+                    No se encontró dicho usuario. Intente nuevamente
+                  </center>
+                </td>
+              </tr>
+              @endforelse
+              @endsection
+              @section('paginacion')
+              <div class="mt-4">
+                {{ $usuarios->links() }}
+              </div>
+              @endsection
+            </x-table>
+          </div>
         </div>
+      </div>
     </div>
-</x-slot>
+  </x-slot>
 </x-app-layout>
