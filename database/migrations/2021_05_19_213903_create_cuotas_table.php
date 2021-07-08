cf1c2367f0aec1c7cd930a6c2d10ccc0052db880
andresbrice <andresbriceno71094@gmail.com>
2021-07-08 20:37:08 -0300

    tabla cuota act.

--- database/migrations/2021_05_19_213903_create_cuotas_table.php
+++ database/migrations/2021_05_19_213903_create_cuotas_table.php
@@ -6,30 +6,31 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateCuotasTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('cuotas', function (Blueprint $table) {
-            $table->engine = 'InnoDB';
-            $table->id();
-            $table->date('fecha_de_pago');
-            $table->float('precio');
-            $table->foreignId('alumno_clase_id')->constrained('alumno_clase');
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('cuotas', function (Blueprint $table) {
+      $table->engine = 'InnoDB';
+      $table->id();
+      $table->date('fecha_de_pago');
+      $table->date('fecha_de_caducidad');
+      $table->float('importe');
+      $table->foreignId('alumno_clase_id')->constrained('alumno_clase');
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('cuotas');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('cuotas');
+  }
 }
