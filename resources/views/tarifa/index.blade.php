5f20a9096842d696bcbd7f6be0d31acb64e4f47e
daianadirie <daydirie@gmail.com>
2021-07-12 18:42:29 -0300

    actualice la gestion

--- resources/views/tarifa/index.blade.php
+++ resources/views/tarifa/index.blade.php
@@ -69,7 +69,7 @@
                                                         <span class="pr-1 font-semibold flex-1">Acciones</span>
                                                         <span>
                                                             <svg class="fill-current h-4 w-4 transform group-hover:-rotate-180
-                                                                                                                                                                                                                    transition duration-150 ease-in-out"
+                                                                                                                                                                                                                        transition duration-150 ease-in-out"
                                                                 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                                                 <path
                                                                     d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
