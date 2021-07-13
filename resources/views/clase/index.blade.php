4b817900bf5501ef2651e879382733f89dfbd454
andresbrice <andresbriceno71094@gmail.com>
2021-07-13 19:46:20 -0300

    modelo clase con busqueda

--- resources/views/clase/index.blade.php
+++ resources/views/clase/index.blade.php
@@ -58,7 +58,7 @@
                 </th>
                 <th scope="col"
                   class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  clase
+                  Horario
                 </th>
                 <th scope="col"
                   class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
