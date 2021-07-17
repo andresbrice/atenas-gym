7b282ab56abca85be604d18de525c38a248c25d8
andresbrice <andresbriceno71094@gmail.com>
2021-07-17 17:51:04 -0300

    agregue mensajes de error en cuota,ejer,horario

--- resources/views/cuota/seleccionaralumno.blade.php
+++ resources/views/cuota/seleccionaralumno.blade.php
@@ -13,7 +13,7 @@
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
           <div class="p-2 2xl:p-4 bg-white border-gray-200">
             <!-- Validation Errors -->
-            {{-- <x-validation-errors class="mb-3" :errors="$errors" /> --}}
+            <x-validation-errors class=" mt-5" :errors="$errors" />
 
             <form action="{{ route('cuota.create') }}" method="GET">
               @csrf
