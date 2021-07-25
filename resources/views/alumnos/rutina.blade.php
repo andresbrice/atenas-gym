d61e5795427e479b182d39e147fdbbd9bfe6929b
Jandrih <julianandrichf@gmail.com>
2021-07-25 16:57:07 -0500

    Rutina en Gestion Alumno

--- resources/views/alumnos/rutina.blade.php
+++ resources/views/alumnos/rutina.blade.php
@@ -43,8 +43,6 @@
                                          @endforeach <br>
                                          {{ $clase->horario->hora->format('H:i A') }}
 
-                                        {{ Auth::user()->name }} {{ Auth::user()->lastName }}
-
                                          {{-- {{$rutina->alumno_clase->alumno->user->name}} {{$rutina->alumno_clase->alumno->user->lastName}}
                                          <br>
                                          {{$rutina->alumno_clase->clase->tipo_clase}}
