0a54dabcf2e67f1f55f815862b39d9d4cd5dbe2d
daianadirie <daydirie@gmail.com>
2021-07-25 23:54:56 -0300

    correccion de mensajes de error en todas las vistas

--- resources/views/clase/index.blade.php
+++ resources/views/clase/index.blade.php
@@ -8,7 +8,7 @@
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
           <div class="p-2 2xl:p-4 bg-white border-b border-gray-200">
-            {{-- <x-success-message class=" mt-5" :errors="$errors" /> --}}
+            <x-success-message class=" mt-5" :errors="$errors" />
             <x-success-message class="mb-4 font-bold flex justify-center" />
             <x-denied-message class="mb-4 font-bold flex justify-center" />
             <div class="mb-3">
