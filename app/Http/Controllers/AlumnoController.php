881aed25c68762737d9d82314fee6f0c50a611e6
andresbrice <andresbriceno71094@gmail.com>
2021-07-26 00:47:21 -0300

    adelanto en mostrar rutina para alumno

--- app/Http/Controllers/AlumnoController.php
+++ app/Http/Controllers/AlumnoController.php
@@ -17,11 +17,11 @@ class AlumnoController extends Controller
 
   public function consultaClase()
   {
-    $clases = Clase::whereHas('alumno_clase', function($query) {
-      $query->whereHas('alumno', function($query) {
-        $query->whereHas('user', function($query) {
+    $clases = Clase::whereHas('alumno_clase', function ($query) {
+      $query->whereHas('alumno', function ($query) {
+        $query->whereHas('user', function ($query) {
           $query->where('id', '=', auth()->id());
-        });  
+        });
       });
     })->get();
 
@@ -30,11 +30,11 @@ class AlumnoController extends Controller
 
   public function buscarClase()
   {
-    $clases = Clase::whereHas('alumno_clase', function($query) {
-      $query->whereHas('alumno', function($query) {
-        $query->whereHas('user', function($query) {
+    $clases = Clase::whereHas('alumno_clase', function ($query) {
+      $query->whereHas('alumno', function ($query) {
+        $query->whereHas('user', function ($query) {
           $query->where('id', '=', auth()->id());
-        });  
+        });
       });
     })->get();
 
@@ -44,41 +44,18 @@ class AlumnoController extends Controller
   public function seleccionarclase(Request $request)
   {
     $clase = Clase::findOrFail($request->tipo_clase);
-    $clase_id = $clase->id;
-    
-    $alumno = Alumno::whereHas('user', function($query) {
-      $query->where('user_id', auth()->id());
-    })->get();
+    $queryAlumnoClase = DB::select('select alumno_clase.id as id from alumno_clase where alumno_clase.id in (select alumno_clase.id from alumno_clase join alumnos on alumno_clase.alumno_id = alumnos.id where alumno_clase.clase_id = ? and alumno_clase.alumno_id in (SELECT alumno_clase.alumno_id from alumno_clase JOIN alumnos on alumno_clase.alumno_id = alumnos.id JOIN users on alumnos.user_id = users.id where users.id = ?))', [$clase->id, auth()->id()]);
 
-    $alumno_id = $alumno->users->id;
-    dd($alumno_id);
+    $alumno_clase = Alumno_Clase::findOrFail($queryAlumnoClase[0]->id);
 
-    // $alumno_clase = Alumno_Clase::where('clase_id', $clase_id)
-    //   ->where('alumno_id', $alumno_id);
-    // dd($alumno_clase);
+    $rutina = Rutina::where('alumno_clase_id', $alumno_clase->id)->get();
 
-    // $rutina = Rutina::where('alumno_clase_id', $alumno_clase)->first();
-    // dd($rutina);
+    dd($rutina);
 
     // $ejercicios_rutina = DB::select('select ejercicios.id as id, ejercicio_rutina.id as ejercicio_rutina_id, ejercicios.nombre_ejercicio as nombre_ejercicio, rutinas.id as rutina_id, ejercicio_rutina.series as series, ejercicio_rutina.repeticiones as repeticiones, ejercicio_rutina.descanso as descanso from ejercicio_rutina join ejercicios on ejercicio_rutina.ejercicio_id = ejercicios.id join rutinas on ejercicio_rutina.rutina_id = rutinas.id where rutina_id = ?', [$rutina->id]);
 
-    // $ejercicios = DB::query()
-    //   ->select('rutinas.id', 'rutinas.fecha_emision', 'clases.tipo_clase', 'ejercicios.nombre_ejercicio', 'ejercicio_rutina.series', 'ejercicio_rutina.repeticiones', 'ejercicio_rutina.descanso')
-    //   ->from('rutinas')
-    //   ->join('ejercicio_rutina', 'rutinas.id', '=', 'ejercicio_rutina.rutina_id')
-    //   ->join('ejercicios', 'ejercicio_rutina.ejercicio_id', '=', 'ejercicios.id')
-    //   ->join('alumno_clase', 'rutinas.alumno_clase_id', '=', 'alumno_clase.id')
-    //   ->join('clases', 'alumno_clase.clase_id', '=', 'clases.id')
-    //   ->join('alumnos', 'alumno_clase.alumno_id', '=', 'alumnos.id')
-    //   ->join('users', 'alumnos.user_id', '=', 'users.id')
-    //   ->where('users.id', '=', auth()->id())
-    //   ->where('clases.id', '=', $clase_id)
-    //   ->groupBy('ejercicio_rutina.id')
-    //   ->first()
-    //   ->get();
-      // dd($ejercicios);
 
-    return view('alumnos.rutina', compact('clase', 'clase_id', 'alumno_id', 'alumno_clase', 'rutina', 'ejercicios_rutina'));
+    return view('alumnos.rutina', compact('rutina', 'ejercicios_rutina'));
   }
 
   public function consultaAsistencia()
@@ -93,6 +70,4 @@ class AlumnoController extends Controller
   {
     return view('alumnos.imprimirRutina');
   }
-
-
 }
